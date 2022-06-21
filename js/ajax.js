8381d795e335580a1391c14be7860488895cde78
Daniel Santos <danielspovv@gmail.com>
2022-06-21 17:47:35 -0300

    refactor: indentation, function names and data structure

--- js/ajax.js
+++ js/ajax.js
@@ -1,40 +1,30 @@
-(function(window){
-
-    let ajax = {};
-
-    function gerRequestObject(){
-        if(XMLHttpRequest){
-            return(new XMLHttpRequest());
-        }else{
-            window.alert('AJAX is not supported :(');
-            return(null);
-        }
+(function(window) {
+    function gerRequestObject() {
+      if(XMLHttpRequest) {
+        return new XMLHttpRequest();
+      } else {
+        window.alert('AJAX is not supported');
+        return null;
+      }
     }
 
-    ajax.makeRequest = function(url, func, isJson){
-        let request = gerRequestObject();
-        request.onreadystatechange = function(){
-            whatToDo(request, func, isJson);
+    function whatToDo(request, func, isJson){
+      if(request.readyState == 4 && request.status == 200) {
+        if(isJson == undefined || isJson) {
+          func(JSON.parse(request.responseText));
+        } else {
+          func(request.responseText)
         }
+      }
+    };
 
+    window.$ajax = {
+      makeRequest: (url, func, isJson) => {
+        const request = gerRequestObject();
+        request.onreadystatechange = () => whatToDo(request, func, isJson);
         request.open('GET', url, true);
         request.send(null);
+      },
     };
-
-    function whatToDo(request, func, isJson){
-        if(request.readyState == 4 && request.status == 200){
-            if(isJson == undefined){
-                isJson = true;
-            }
-
-            if(isJson){
-                func(JSON.parse(request.responseText));
-            }else{
-                func(request.responseText)
-            }
-        }
-    };
-
-    window.$ajax = ajax;
 
 })(window);
---
+++ js/data/index.js
@@ -0,0 +1,17 @@
+(function(window) {
+    window.$data = {
+      tabs: {
+        profile: 'li-profile',
+        done: 'li-done',
+        career: 'li-career',
+      },
+      ids: {
+        profile: '#profile',
+        brand: '#brand',
+        logo: '#logo-link',
+        career: '#career',
+        done: '#done',
+      }
+    };
+
+})(window);
